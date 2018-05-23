f6bc77018343867b48cb3ed2edd4728b0d722292
Martin Sileno <maartin.sileno@gmail.com>
2018-05-23 20:54:50 +0200

    Gzip option in PubgAPI constructor now defaults to false
    Browsers sending requests must not set this option to true as it will cause an error.

--- src/api/base.ts
+++ src/api/base.ts
@@ -7,13 +7,19 @@ import { PlatformRegion } from '../shared/constants';
  * Instances of this class will be used to make authenticated requests to the PUBG API.
  *
  * Instantiate this by providing your API key and the PlatformRegion.
+ *
+ * It accepts an optional `useGzip` option which, if set to true, requests that the server
+ * compress its response using gzip. This can improve performance for large responses such as
+ * matches.
+ * The `useGzip` option must NOT be set to true if using a browser to send requests as it handles
+ * the `Accept-Encoding` header by itself.
  */
 export class PubgAPI {
   private _axios: AxiosInstance;
   private _apiKey: string;
   private _platformRegion: PlatformRegion;
 
-  constructor(apiKey: string, platformRegion: PlatformRegion, useGzip = true) {
+  constructor(apiKey: string, platformRegion: PlatformRegion, useGzip = false) {
     this._apiKey = apiKey;
     this._platformRegion = platformRegion;
     const headers: {[p: string]: string} = {
