4c9e0ec8ac8f08cf4f578e0ca467158446c6bf90
Thomas Ortiz <tdortiz@ncsu.edu>
2018-12-08 18:56:16 -0500

    Added 'PSN' to PlatformRegion enum and organized the enum

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -20,23 +20,32 @@ export enum MapName {
 }
 
 export enum PlatformRegion {
+  // PC Platforms
   STEAM = 'steam',
   KAKAO = 'kakao',
+  PC_AS = 'pc-as',
+  PC_EU = 'pc-eu',
+  PC_JP = 'pc-jp',
+  PC_KRJP = 'pc-krjp',
+  PC_KAKAO = 'pc-kakao',
+  PC_NA = 'pc-na',
+  PC_OC = 'pc-oc',
+  PC_RU = 'pc-ru',
+  PC_SA = 'pc-sa',
+  PC_SEA = 'pc-sea',
+  // PSN Platforms
+  PSN = 'psn',
+  PSN_AS = 'psn-as',
+  PSN_EU = 'psn-eu',
+  PSN_NA = 'psn-na',
+  PSN_OC = 'psn-oc',
+  // XBOX Platforms
   XBOX = 'xbox',
   XBOX_AS = 'xbox-as',
   XBOX_EU = 'xbox-eu',
   XBOX_NA = 'xbox-na',
   XBOX_OC = 'xbox-oc',
-  XBOX_SA = 'xbox-sa',
-  PC_KRJP = 'pc-krjp',
-  PC_JP = 'pc-jp',
-  PC_NA = 'pc-na',
-  PC_EU = 'pc-eu',
-  PC_OC = 'pc-oc',
-  PC_KAKAO = 'pc-kakao',
-  PC_SEA = 'pc-sea',
-  PC_SA = 'pc-sa',
-  PC_AS = 'pc-as',
+  XBOX_SA = 'xbox-sa'
 }
 
 export enum SeasonState {
